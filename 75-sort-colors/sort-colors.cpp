ce7bce1714fb8057a1a2567568320c5a8ccb4a2c
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-05-27 11:07:03 +0530

    Time: 2 ms (43.61%) | Memory: 10 MB (5.41%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        map<int, int>m;
+        for(int i = 0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        nums = {};
+        for(auto i : m){
+            for(int k = 0; k<i.second; k++){
+                nums.push_back(i.first);
+            }
+        }
+
         
     }
 };
