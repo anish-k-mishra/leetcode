629aa7fe9b770f2bfea9e76264809bdebfae49da
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-06-16 11:31:17 +0530

    Time: 3 ms (55.23%) | Memory: 7.5 MB (35.35%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,33 +1,24 @@
-#include <vector>
-#include <string>
-
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        // Factorials from 0! to (n-1)!
-        vector<int> factorial(n, 1);
-        for (int i = 1; i < n; ++i) {
-            factorial[i] = factorial[i - 1] * i;
+        int fact = 1;
+        vector<int>v;
+        for(int i = 1; i<n; i++){
+            fact = fact*i;
+            v.push_back(i);
         }
-        
-        // Numbers from 1 to n
-        vector<int> nums;
-        for (int i = 1; i <= n; ++i) {
-            nums.push_back(i);
+        v.push_back(n);
+        string ans = "";
+        k = k-1;
+        while(true){
+            ans = ans+ to_string(v[k/fact]);
+            v.erase(v.begin() + k/fact);
+            if(v.size() == 0){
+                break;
+            }
+            k = k%fact;
+            fact = fact/v.size();
         }
-        
-        // Adjust k to be zero-indexed
-        k--;
-
-        // Generate the k-th permutation
-        string result;
-        for (int i = n; i >= 1; --i) {
-            int j = k / factorial[i - 1];
-            result.push_back('0' + nums[j]);
-            nums.erase(nums.begin() + j);
-            k %= factorial[i - 1];
-        }
-        
-        return result;
+        return ans;
     }
 };
