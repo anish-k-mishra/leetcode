801450a87542908cb3f0738fe313edfb7479bda4
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-07-23 14:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (29.58%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool solve(string& s, int ind, int cnt, vector<vector<int>>&dp){
-        if(cnt<0){
-            return false;
-        }
-        if(ind == s.length()){
-            if(cnt == 0){
-                return true;
+    bool checkValidString(string s) {
+        int min = 0, max = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s[i] == '('){
+                max++;
+                min++;
+            }
+            else if(s[i] == ')'){
+                max--;
+                min--;
             }
             else{
+                min--;
+                max++;
+            }
+
+            if(min<0){
+                min = 0;
+            }
+            if(max<0){
                 return false;
             }
         }
-        if(dp[ind][cnt] != -1){
-            return dp[ind][cnt];
+        if(min == 0){
+            return true;
         }
-
-        if(s[ind] == '('){
-            return dp[ind][cnt]= solve(s, ind+1, cnt+1, dp);
-        }
-        else if(s[ind] == ')'){
-            return dp[ind][cnt] = solve(s, ind+1, cnt-1, dp);
-        }
-        else{
-            return dp[ind][cnt] = (solve(s, ind+1, cnt+1, dp) || solve(s, ind+1, cnt-1, dp) || solve(s, ind+1, cnt, dp));
-        }
-    }
-    bool checkValidString(string s) {
-        int cnt = 0;
-        int ind = 0;
-        vector<vector<int>>dp(s.length()+1, vector<int>(s.length()+1, -1));
-        return solve(s, ind, cnt, dp);
+        return false;
     }
 };
