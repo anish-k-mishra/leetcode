e9cb0773e8b7cb87c16e26d8355158146d86d297
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-10-05 10:31:44 +0530

    Time: 3 ms (94.61%) | Memory: 8.6 MB (92.55%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool checkEqual(int a[26], int b[26]){
+        for(int i = 0; i<26; i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        //character count array
+        int count1[26] = {0};
+        for(int i = 0; i<s1.length(); i++){
+            int index = s1[i] - 'a';
+            count1[index]++; 
+        }
+        //traverse s2 string in window of s1 length and compare
+        int i = 0;
+        int windowSize = s1.length();
+        int count2[26] = {0};
+
+        //running for first window
+        while(i<windowSize && i<s2.length()){
+            int index = s2[i] - 'a';
+            count2[index]++;
+            i++;
+        }
+        if(checkEqual(count1, count2)){
+            return true;
+        }
+
+        //aage window process karo
+        while(i<s2.length()){
+            char newChar = s2[i];
+            int index = newChar - 'a';
+            count2[index]++;
+            char oldChar = s2[i-windowSize];
+            index = oldChar - 'a';
+            count2[index]--;
+            i++;
+            if(checkEqual(count1, count2)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+};
