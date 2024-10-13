74721607ae797a8db94d74ffc93708ff634a5fc1
Anish Mishra <107322619+anish-k-mishra@users.noreply.github.com>
2024-10-13 19:58:26 +0530

    Time: 4 ms (47.06%) | Memory: 8 MB (17.93%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int ans = 0;
-        bool lastsp = false;
         bool wordenc = false;
         for(int i = s.length()-1; i>=0; i--){
             if(s[i] == ' ' && !wordenc){
-                lastsp = true;
                 continue;
             }
             else if(s[i] != ' '){
